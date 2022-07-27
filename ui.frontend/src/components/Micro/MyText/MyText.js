de5e642d8aabcc507776569d8d65070fea3da675
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-27 20:00:17 -0300

    Style: adding a div text

--- ui.frontend/src/components/Micro/MyText/MyText.js
+++ ui.frontend/src/components/Micro/MyText/MyText.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { MapTo } from "@adobe/aem-react-editable-components";
-import { StyledText } from "./MyText.styled";
+import { StyledText,TextDiv } from "./MyText.styled";
 
 export const MyText = (props) => {
     const mytextdefalt = "Digite um texto aqui por favor.";
   return (
+    <TextDiv>
     <StyledText>{props.text ? props.text : mytextdefalt}</StyledText>
+    </TextDiv>
   )
 };
 
--- ui.frontend/src/components/Micro/MyText/MyText.styled.js
+++ ui.frontend/src/components/Micro/MyText/MyText.styled.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
+export const TextDiv = styled.div`
+    width: 19.1rem;
+`;
+
 export const StyledText = styled.p`
     font-family: 'Space Mono', monospace;
     color: #4F4F4F;
     font-weight: 400;
-    font-size: 0.875rem;
-    line-height:2.25rem;
+    font-size: 1.2rem;
+    line-height: 1.8rem;
 `;
