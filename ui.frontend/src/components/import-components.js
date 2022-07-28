789fc53ca042e5090e3621f3a6a7a11c83e8afc6
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-07-28 15:00:50 -0300

    Feat: creating the header component

--- ui.frontend/src/components/import-components.js
+++ ui.frontend/src/components/import-components.js
@@ -23,6 +23,7 @@ import MeuComponente from "./MeuComponente";
 import "./Micro/MyText/MyText";
 import "./Micro/MyButton2/MyButton2";
 import "./Micro/MyTitle/MyTitle"
+import "./MyHeader/MyHeader";
 
 import { MapTo } from "@adobe/aem-react-editable-components";
 
