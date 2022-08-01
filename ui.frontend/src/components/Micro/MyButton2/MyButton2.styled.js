043b3fd67abbd5740bb121caa1dfbc3fc53bf0cc
Fernanda Torres <fernanda.sttorres@gmail.com>
2022-08-01 17:32:46 -0300

    Style: changing the components size

--- ui.frontend/src/components/Micro/MyButton2/MyButton2.styled.js
+++ ui.frontend/src/components/Micro/MyButton2/MyButton2.styled.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export const StyledButton = styled.button`
     background: #333333;
-    width: 13.5rem;
-    height: 4.25rem;
+    width: 12.15rem;
+    height: 3.825rem;
     border: none;
     color: #FFFFFF;
     font-family: 'Space Mono';
     font-weight: 700;
-    font-size: 0.875rem;
-    margin-top: 4rem;
+    font-size: 0.8rem;
+    margin-top: 2.5rem;
 `;
